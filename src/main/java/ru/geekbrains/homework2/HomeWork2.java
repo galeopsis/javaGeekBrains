9078d257d1d2246be64316259b5877453a2b579f
Roman S. Nesterov <galeopsis@mail.ru>
2021-02-10 22:33:32 +0700

    Some optimizations )

--- src/main/java/ru/geekbrains/homework2/HomeWork2.java
+++ src/main/java/ru/geekbrains/homework2/HomeWork2.java
@@ -3,14 +3,7 @@ package ru.geekbrains.homework2;
 public class HomeWork2 {
 
     public static void main(String[] args) {
-        test(-45654);
-        test(654);
-        test(-54);
-        test(-45654);
+
     }
 
-    private static void test(int i) {
-        String word = (i > 0) ? ("положительное!") : ("отрицательное!");
-        System.out.printf("Число %d %s %n", i, word);
-    }
 }
