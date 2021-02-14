83f3e34393a2fb48c194f277e2ee6ea5c7bc2969
Roman S. Nesterov <galeopsis@mail.ru>
2021-02-14 20:14:33 +0700

    Готово!

--- src/main/java/ru/geekbrains/homework2/HomeWork2.java
+++ src/main/java/ru/geekbrains/homework2/HomeWork2.java
@@ -1,43 +1,39 @@
 package ru.geekbrains.homework2;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class HomeWork2 {
+
     public static void main(String[] args) {
+
+        final int SIZE = 8;
 
         //Task #1
         System.out.println("Task #1: ");
-        final int SIZE = 9;
-        int[] data = new int[SIZE];
-        for (int i = 0 ; i < data.length; i++) {
-            data[i] = (int)(Math.random() * 2);
-        }
+
+        int[] data = IntStream.range(0, SIZE).map(i -> (int) (Math.random() * 2)).toArray();
         System.out.println(Arrays.toString(data));
-        for(int i = 0 ; i < data.length; i++) {
-            if (data[i] == 0) {
-                data [i] = 1;
-            } else {
-                data [i] = 0;
-            }
+        for (int i = 0; i < data.length; i++) {
+            data[i] = (data[i] == 0) ? 1 : 0;
         }
         System.out.println(Arrays.toString(data));
 
         //Task #2
         System.out.println("Task #2: ");
-        final int SIZE_2 = 8;
-        int[] data_2 = new int[SIZE_2];
+
+        int[] data_2 = new int[SIZE];
         int a = 0;
-        for (int i = 0 ; i < data_2.length; i++) {
+        for (int i = 0; i < data_2.length; i++) {
             data_2[i] = a;
-            a+=3;
+            a += 3;
             System.out.printf("%3d ", data_2[i]);
         }
         System.out.println();
 
        /* Вариант Макса (для понимания процесса!)
 
-        final int SIZE_2 = 8;
-        int[] data_2 = new int[SIZE_2];
+        int[] data_2 = new int[SIZE];
         for (int i = 0 ; i < data_2.length; i++) {
             data_2[i] = i * 3;
             System.out.printf("%3d ", data_2[i]);
@@ -46,12 +42,13 @@ public class HomeWork2 {
 
         //Task #3
         System.out.println("Task #3: ");
-        int[] data_3 = {1,5,3,2,11,4,5,2,4,8,9,1};
+
+        int[] data_3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         System.out.println(Arrays.toString(data_3));
         for (int i = 0; i < data_3.length; i++) {
             if (data_3[i] < 6) {
-               int d = data_3[i] * 2;
-                data_3 [i] = d;
+                int d = data_3[i] * 2;
+                data_3[i] = d;
             }
 
         }
@@ -59,89 +56,94 @@ public class HomeWork2 {
 
         //Task #4
         System.out.println("Task #4: ");
-        final int SIZE_3 = 3;
-        int[][] deepData = new int[SIZE_3][SIZE_3];
-        int n = 0;
+
+        int[][] deepData = new int[SIZE][SIZE];
         for (int i = 0; i < deepData.length; i++) {
-            for (int j = 0; j < deepData[i].length; j++) {
-                deepData[i][j] = ++n;
-            }
+            deepData[i][i] = deepData[i][(deepData.length - 1) - i] = 1;
         }
-        for (int i = 0; i < deepData.length; i++) {
-            for (int j = 0; j < deepData[i].length; j++) {
-                System.out.printf("%3d", deepData[i][j]);
-
-
+        for (int[] myArray : deepData) {
+            for (int i : myArray) {
+                System.out.printf("%3d", i);
             }
             System.out.println();
         }
 
-        /*for(int i = 0; i <22; i+=3) {
-            data2[i] = i;
-        }
-        System.out.printf("%3d", data2);*/
+        //Task #5
+        System.out.println("Task #5: ");
 
-        /*for (int i = 20; i > 0; i--){
-
-
-            if (i % 2 == 0) {
-                continue;
+        int[] array1 = IntStream.range(0, SIZE).map(i -> (int) (Math.random() * 100)).toArray();
+        System.out.println(Arrays.toString(array1));
+        int min = array1[0];
+        int max = array1[0];
+        for (int result : array1) {
+            if (min > result) {
+                min = result;
+            } else if (max < result) {
+                max = result;
             }
-            System.out.println(i);
-        }*/
-            /*int i = 1;
-            while (i < 20) {
-                i++;
-                if (i % 2 == 0) {
-                    continue;
+        }
+        System.out.println("Наибольшее число в данном массиве = " + max);
+        System.out.println("Наименьшее число в данном массиве = " + min);
+
+        //Task #6
+        System.out.println("Task #6: ");
+
+        int[] balanceArr = IntStream.range(0, 8).map(i -> (int) (Math.random() * 2)).toArray();//генерируем для простоты массив со случайными 0 и 1
+        System.out.println(Arrays.toString(balanceArr));//выводим полученный массив в консоль
+        System.out.println("Результат = " + checkBalance(balanceArr));//ищем баланс
+
+        //Task #7
+        System.out.println("Task #7: ");
+
+        int[] shiftArr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(Arrays.toString(shiftArr));//исходный массив
+        int n = -3;//отрицательное для смещения влево, положительное для смещения вправо
+
+        shiftArrayRight(shiftArr, n);
+
+    }
+
+    private static void shiftArrayRight(int[] shiftArr, int n) {
+        int l = shiftArr.length;
+        if (n >= 0) {
+            for (int i = 0; i < Math.abs(n); i++) {
+                int lastIndex = shiftArr[l - 1];
+                for (int j = l - 1; j > 0; j--) {
+                    shiftArr[j] = shiftArr[j - 1];
                 }
-                System.out.println(i);
-            }*/
-       /* for(int j = 1; j < 21; j++){
-
-                System.out.println("Cлучайное число № " + j + " = " + (int)(Math.random() * 1000));
+                shiftArr[0] = lastIndex;
             }
+            System.out.println("Массив будет смещён на " + n + " элементов");
+            System.out.println(Arrays.toString(shiftArr));
+        } else {
+        for (int i = 0; i < Math.abs(n); i++) {
+            int startIndex = shiftArr[0];
+            for (int j = 0; j < l - 1; j++) {
+                shiftArr[j] = shiftArr[j + 1];
+            }
+            shiftArr[l - 1] = startIndex;
+        }
+        System.out.println("Массив будет смещён на " + n + " элементов");
+        System.out.println(Arrays.toString(shiftArr));
+    }
 
-            int i = 0;
-            while (i < 20) {
-                i++;
-                System.out.println("Cлучайное число № " + i + " = " + (int)(Math.random() * 1000));
-            }*/
+}
 
-      //  final int SIZE = 7;
-       /* int[] data = new int[SIZE];
 
-        data[0] = 3;
-        data[1] = 7;
-        data[2] = 2;
-        data[3] = 6;
-        data[4] = 8;*/
-
-/*//        System.out.println(data[1]);
-
-        for (int i = 0 ; i < data.length; i++) {
-            System.out.println(data[i]);
-        }*/
-       /* Arrays.sort(data);
-        System.out.println(Arrays.toString(data));
-        System.out.println(Arrays.toString(new int[]{1,2,3,4,5,6,7}));
-        for(int a = 0; a <22; a+=3){
-            System.out.println(a);
-        }*/
-       /* int[][] deepData = new int[SIZE][SIZE];
-        int n = 0;
-        for (int i = 0; i < deepData.length; i++) {
-            for (int j = 0; j < deepData[i].length; j++) {
-                deepData[i][j] = ++n;
+    public static boolean checkBalance(int[] balanceArr) {
+        boolean isBalanced = false;
+        int sumL = 0;
+        int sumR = Arrays.stream(balanceArr).sum();
+        for (int j : balanceArr) {
+            sumL += j;
+            sumR -= j;
+            if (sumL == sumR) {
+                isBalanced = true;
+            } else if (sumL > sumR) {
+                return isBalanced;
             }
         }
-        for (int i = 0; i < deepData.length; i++) {
-            for (int j = 0; j < deepData[i].length; j++) {
-                System.out.printf("%3d", deepData[i][j]);
-
-
-            }
-            System.out.println();
-        }*/
+        return isBalanced;
     }
 }
+
