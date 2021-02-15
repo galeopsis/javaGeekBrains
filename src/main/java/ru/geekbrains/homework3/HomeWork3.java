6f079f96c11af0a2e01529d6b53be9782a670ec5
Roman S. Nesterov <galeopsis@mail.ru>
2021-02-15 20:50:59 +0700

    Full working version

---
+++ src/main/java/ru/geekbrains/homework3/HomeWork3.java
@@ -0,0 +1,56 @@
+package ru.geekbrains.homework3;
+
+import java.util.Scanner;
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+        //Task #1
+        System.out.println("Task #1: ");
+        int secret = (int) (Math.random() * 10);
+        askSecret(secret);
+    }
+
+    private static void askSecret(int secret) {
+        int attempts = 3;
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            if (attempts == 0) {
+                System.out.print("Вы проиграли! ");
+                System.out.print("Хотите сыграть снова? 1 = Да, Любое другое число = Нет ");
+                if (in.nextInt() == 1) {
+                    secret = (int) (Math.random() * 10);
+                    System.out.println(secret);
+                    askSecret(secret);
+                }
+                break;
+            }
+            System.out.print("Угадайте число от 0 до 10: ");
+            int answer = in.nextInt();
+            attempts--;
+            if (answer == secret) {
+                System.out.print("Поздравляю, Вы угадали! ");
+                System.out.print("Хотите сыграть снова? 1 = Да, Любое другое число = Нет ");
+                if (in.nextInt() == 1) {
+                    secret = (int) (Math.random() * 10);
+                    System.out.println(secret);
+                    askSecret(secret);
+                }
+                break;
+            }
+            if (answer < secret) {
+                System.out.println("Ваш ответ слишком маленький. ");
+            } else if (answer > secret) {
+                System.out.println("Ваш ответ слишком большой. ");
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+
+
+
+
+
